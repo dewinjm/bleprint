3af82af9711372d0763c320b4c0f499f2ee43376
dewinjm <dewin.martinez@gmail.com>
2022-08-02 18:46:03 -0400

    fix(example): modify integration_test

--- bleprint/example/integration_test/app_test.dart
+++ bleprint/example/integration_test/app_test.dart
@@ -19,9 +19,9 @@ void main() {
       app.main();
       await tester.pumpAndSettle();
       expect(find.text('BLE Print Example'), findsOneWidget);
-      // await tester.pumpAndSettle();
+      await tester.pumpAndSettle();
       // final expected = expectedPlatformName();
-      // await tester.ensureVisible(find.text('Platform Name: $expected'));
+      await tester.ensureVisible(find.text('BLE Print Example'));
     });
   });
 }
