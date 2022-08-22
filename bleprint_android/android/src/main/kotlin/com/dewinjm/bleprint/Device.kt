d24e2c06ee2e8a9b128ea47d3d2d169493f9f25f
dewinjm <dewin.martinez@gmail.com>
2022-08-22 11:45:55 -0400

    chore(android): modify Device model, add state field

--- bleprint_android/android/src/main/kotlin/com/dewinjm/bleprint/Device.kt
+++ bleprint_android/android/src/main/kotlin/com/dewinjm/bleprint/Device.kt
@@ -1,9 +1,13 @@
 package com.dewinjm.bleprint
 
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothProfile
 
 object Device {
-    fun toJson(device: BluetoothDevice): MutableMap<String, Any>? {
+    fun toJson(
+        device: BluetoothDevice,
+        state: Int? = BluetoothProfile.STATE_DISCONNECTED
+    ): MutableMap<String, Any>? {
         if (device.name == null)
             return null
 
@@ -11,6 +15,7 @@ object Device {
         map["address"] = device.address
         map["name"] = device.name
         map["type"] = device.type
+        map["state"] = state!!
         return map
     }
 }
